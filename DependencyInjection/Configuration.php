36b4cba0803db43e49b960acbce446ac290638bc
Nikolay Georgiev <azazen09@gmail.com>
2012-10-08 13:29:39 +0300

    added configurations and dependancy injections

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -4,26 +4,92 @@ namespace Neutron\Plugin\TeamMemberBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 
-/**
- * This is the class that validates and merges configuration from your app/config files
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
- */
 class Configuration implements ConfigurationInterface
 {
-    /**
-     * {@inheritDoc}
-     */
+
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('neutron_team_member');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
         return $treeBuilder;
     }
+    
+    private function addGeneralConfigurations(ArrayNodeDefinition $node)
+    {
+        $node
+            ->addDefaultsIfNotSet()
+            ->children()
+                ->booleanNode('enable')->defaultFalse()->end()
+                ->scalarNode('overview_class')->isRequired()->cannotBeEmpty()->end()
+                ->scalarNode('team_member_reference_class')->isRequired()->cannotBeEmpty()->end()
+                ->scalarNode('overview_manager')->defaultValue('neutron_team_member.doctrine.orm.overview_manager.default')->end()
+                ->scalarNode('overview_controller_backend')->defaultValue('neutron_team_member.controller.backend.overview.default')->end()
+                ->scalarNode('overview_controller_frontend')->defaultValue('neutron_team_member.controller.frontend.overview.default')->end()
+                ->scalarNode('team_member_class')->isRequired()->cannotBeEmpty()->end()
+                ->scalarNode('team_member_manager')->defaultValue('neutron_team_member.doctrine.orm.team_member_manager.default')->end()
+                ->scalarNode('team_member_controller_backend')->defaultValue('neutron_team_member.controller.backend.team_member.default')->end()
+                ->scalarNode('team_member_controller_frontend')->defaultValue('neutron_team_member.controller.frontend.team_member.default')->end()
+                ->scalarNode('team_member_datagrid_management')->defaultValue('team_member_management')->end()
+                ->scalarNode('translation_domain')->defaultValue('NeutronTeamMemberBundle')->end()
+            ->end()
+        ;
+    }
+    
+    private function addOverviewFormConfigurations(ArrayNodeDefinition $node)
+    {
+        $node
+            ->children()
+                 ->arrayNode('overview_form')
+                    ->addDefaultsIfNotSet()
+                        ->children()
+                            ->scalarNode('type')->defaultValue('neutron_team_member_overview')->end()
+                            ->scalarNode('handler')->defaultValue('neutron_team_member.form.handler.overview.default')->end()
+                            ->scalarNode('name')->defaultValue('neutron_team_member_overview')->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+    }
+    
+    private function addOverviewTemplatesConfigurations(ArrayNodeDefinition $node)
+    {
+        $node
+            ->children()
+                ->arrayNode('overview_templates')->isRequired()
+                ->validate()
+                    ->ifTrue(function($v){return empty($v);})
+                    ->thenInvalid('You should provide at least one template.')
+                ->end()
+                ->useAttributeAsKey('name')
+                    ->prototype('scalar')
+                ->end() 
+                ->cannotBeOverwritten()
+                ->isRequired()
+                ->cannotBeEmpty()
+            ->end()
+        ;
+    }
+    
+    private function addTeamMemberFormConfigurations(ArrayNodeDefinition $node)
+    {
+        $node
+            ->children()
+                 ->arrayNode('team_member_form')
+                    ->addDefaultsIfNotSet()
+                        ->children()
+                            ->scalarNode('type')->defaultValue('neutron_team_member')->end()
+                            ->scalarNode('handler')->defaultValue('neutron_team_member.form.handler.team_member.default')->end()
+                            ->scalarNode('name')->defaultValue('neutron_team_member')->end()
+                            ->scalarNode('datagrid')->defaultValue('neutron_team_member_form')->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+    }
+
 }
--- DependencyInjection/NeutronTeamMemberExtension.php
+++ DependencyInjection/NeutronTeamMemberExtension.php
@@ -23,6 +23,45 @@ class NeutronTeamMemberExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        
+        if ($config['enable'] === false){
+            $container->getDefinition('neutron_team_member.plugin')->clearTag('neutron.plugin');
+            return;
+        }
+        
+        foreach (array('overview', 'team_member') as $basename) {
+            $loader->load(sprintf('%s.xml', $basename));
+        }
+        
+        $container->setParameter('neutron_team_member.translation_domain', $config['translation_domain']);
+           
+    }
+    
+    protected function loadOverviewConfigurations(array $config, ContainerBuilder $container)
+    {
+        $container->setAlias('neutron_team_member.controller.backend.overview', $config['overview_controller_backend']);
+        $container->setAlias('neutron_team_member.controller.frontend.overview', $config['overview_controller_frontend']);
+        $container->setAlias('neutron_team_member.overview_manager', $config['overview_manager']);
+        $container->setParameter('neutron_team_member.overview_templates', $config['overview_templates']);
+        $container->setParameter('neutron_team_member.overview_class', $config['overview_class']);
+        $container->setParameter('neutron_team_member.team_member_reference_class', $config['team_member_reference_class']);
+        
+        $container->setAlias('neutron_team_member.form.handler.overview', $config['overview_form']['handler']);
+        $container->setParameter('neutron_team_member.form.type.overview', $config['overview_form']['type']);
+        $container->setParameter('neutron_team_member.form.name.overview', $config['overview_form']['name']);
+    }
+    
+    protected function loadTeamMemberConfigurations(array $config, ContainerBuilder $container)
+    {
+        $container->setParameter('neutron_team_member.team_member_class', $config['team_member_class']);
+        $container->setAlias('neutron_team_member.controller.backend.team_member', $config['team_member_controller_backend']);
+        $container->setAlias('neutron_team_member.controller.frontend.overview', $config['team_member_controller_frontend']);
+        $container->setAlias('neutron_team_member.team_member_manager', $config['team_member_manager']);
+        $container->setParameter('neutron_team_member.datagrid.team_member_management', $config['team_member_datagrid_management']);
+        
+        $container->setAlias('neutron_team_member.form.handler.team_member', $config['team_member_form']['handler']);
+        $container->setParameter('neutron_team_member.form.type.team_member', $config['team_member_form']['type']);
+        $container->setParameter('neutron_team_member.form.name.team_member', $config['team_member_form']['name']);
+        $container->setParameter('neutron_team_member.form.datagrid.team_member', $config['team_member_form']['datagrid']);
     }
 }
